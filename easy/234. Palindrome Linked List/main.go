5d36eee7cf09558b84faf60bd576f772d8764e7d
Артем Чаадаев <perfectgentleman.de@ya.ru>
2022-11-14 02:43:17 +0400

    added one more solution

---
+++ easy/234. Palindrome Linked List/main.go
@@ -0,0 +1,91 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// for debug
+func insert(items []int) *ListNode {
+	if len(items) == 0 {
+		return nil
+	}
+
+	firstNode := &ListNode{
+		Val:  items[0],
+		Next: nil,
+	}
+
+	curNode := firstNode
+	for i := 1; i < len(items); i++ {
+		nextNode := &ListNode{
+			Val:  items[i],
+			Next: nil,
+		}
+		curNode.Next = nextNode
+		curNode = nextNode
+	}
+
+	return firstNode
+}
+func show(l *ListNode) {
+	if l == nil {
+		return
+	}
+
+	p := l
+	for p != nil {
+		fmt.Printf("-> %v ", p.Val)
+		p = p.Next
+	}
+	fmt.Print("\n")
+}
+
+// solution functions
+func invert(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	curr := head.Next
+	prev := &ListNode{
+		Val: head.Val,
+	}
+	for curr != nil {
+		prev = &ListNode{
+			Val:  curr.Val,
+			Next: prev,
+		}
+		curr = curr.Next
+	}
+
+	return prev
+}
+func isPalindrome(head *ListNode) bool {
+	ass := invert(head)
+	show(ass)
+
+	p1, p2 := head, ass
+	for p1 != nil {
+		fmt.Printf("p1: %v, p2: %v\n", p1.Val, p2.Val)
+		if p1.Val != p2.Val {
+			return false
+		}
+
+		p1 = p1.Next
+		p2 = p2.Next
+	}
+
+	return true
+}
+
+func main() {
+	ln1 := insert([]int{1, 2, 2, 1})
+	show(ln1)
+	fmt.Println(isPalindrome(ln1))
+	ln2 := insert([]int{1, 2})
+	show(ln2)
+	fmt.Println(isPalindrome(ln2))
+}
