887c65fa57f0628e23e4c8c75b3f9c726f149ad1
Артем Чаадаев <perfectgentleman.de@ya.ru>
2022-12-17 01:36:56 +0400

    added another solution for 744

--- easy/744. Find Smallest Letter Greater Than Target/main.go
+++ easy/744. Find Smallest Letter Greater Than Target/main.go
@@ -24,6 +24,20 @@ func nextGreatestLetter(letters []byte, target byte) byte {
 	return curSym
 }
 
+func nextGreatestLetterAnother(letters []byte, target byte) byte {
+	l, r := 0, len(letters)-1
+	for l <= r {
+		m := l + (r-l)/2
+		if letters[m] <= target {
+			l = m + 1
+		} else {
+			r = m - 1
+		}
+	}
+
+	return letters[l%len(letters)]
+}
+
 func main() {
 	fmt.Println(string(nextGreatestLetter([]byte("cfj"), byte('j'))))
 }
