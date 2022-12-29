2b18639e8d888e5043f2f9bb9aa3a660876060be
Артем Чаадаев <perfectgentleman.de@ya.ru>
2022-12-29 13:45:21 +0400

    added 136

---
+++ easy/136. Single Number/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+)
+
+func TesSingleNumber(t *testing.T) {
+	type testCase struct {
+		Nums           []int
+		ExpectedResult int
+	}
+
+	cases := []testCase{
+		{
+			Nums:           []int{2, 2, 1},
+			ExpectedResult: 1,
+		},
+		{
+			Nums:           []int{4, 1, 2, 1, 2},
+			ExpectedResult: 4,
+		},
+		{
+			Nums:           []int{1},
+			ExpectedResult: 1,
+		},
+	}
+
+	for i := range cases {
+		got := singleNumber(cases[i].Nums)
+		if got != cases[i].ExpectedResult {
+			t.Errorf("Nums: %v, expected: %d, got: %d", cases[i].Nums, cases[i].ExpectedResult, got)
+		}
+	}
+}
---
+++ easy/136. Single Number/readme.md
@@ -0,0 +1,4 @@
+## 69. Sqrt(x)
+
+Description:  
+https://leetcode.com/problems/single-number/
