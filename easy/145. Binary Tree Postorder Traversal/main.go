3b8e43256876b42b9d792202ee307777ab3544de
Артем Чаадаев <perfectgentleman.de@ya.ru>
2023-01-25 17:59:48 +0400

    I hate this. 145 iterative added. Pain in the arse solution

--- easy/145. Binary Tree Postorder Traversal/main.go
+++ easy/145. Binary Tree Postorder Traversal/main.go
@@ -25,6 +25,62 @@ func postorderTraversal(root *TreeNode) []int {
 	return output
 }
 
+type Stack []TreeNode
+
+func (s Stack) IsEmpty() bool {
+	return len(s) == 0
+}
+
+func (s *Stack) Push(node TreeNode) {
+	*s = append(*s, node)
+}
+
+func (s *Stack) Pop() (TreeNode, bool) {
+	if s.IsEmpty() {
+		return TreeNode{}, false
+	} else {
+		index := len(*s) - 1
+		element := (*s)[index]
+		*s = (*s)[:index]
+		return element, true
+	}
+}
+
+func reverseSlice(s []int) []int {
+	l, r := 0, len(s)-1
+
+	for l <= r {
+		s[l], s[r] = s[r], s[l]
+		l++
+		r--
+	}
+
+	return s
+}
+func postorderTraversalIterative(root *TreeNode) []int {
+	res := make([]int, 0)
+	s := make(Stack, 0)
+
+	if root == nil {
+		return nil
+	}
+	s.Push(*root)
+
+	for !s.IsEmpty() {
+		node, _ := s.Pop()
+		res = append(res, node.Val)
+
+		if node.Left != nil {
+			s.Push(*node.Left)
+		}
+		if node.Right != nil {
+			s.Push(*node.Right)
+		}
+	}
+
+	return reverseSlice(res)
+}
+
 func main() {
 	root := TreeNode{Val: 1}
 	root.Right = &TreeNode{Val: 2}
