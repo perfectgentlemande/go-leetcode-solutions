92fda3ab4f7271198db64e3a2ae60a46b26cff79
Артем Чаадаев <perfectgentleman.de@ya.ru>
2023-02-01 23:48:51 +0400

    fx 100

--- easy/100. Same Tree/main.go
+++ easy/100. Same Tree/main.go
@@ -21,7 +21,7 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
 		return false
 	}
 
-	return (p.Val == q.Val) && isSameTree(p.Left, q.Left) && isSameTree(p.Right, p.Right)
+	return (p.Val == q.Val) && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
 }
 
 func main() {
