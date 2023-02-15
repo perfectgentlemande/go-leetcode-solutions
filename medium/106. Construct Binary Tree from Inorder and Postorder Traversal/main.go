90569f850326a109863edf3c79bc765d58ddafc4
Артем Чаадаев <perfectgentleman.de@ya.ru>
2023-02-15 13:07:36 +0400

    added 106 but I don't give a hell how it works

---
+++ medium/106. Construct Binary Tree from Inorder and Postorder Traversal/main.go
@@ -0,0 +1,40 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func helper(inLeft, inRight int, postorder []int, postIdx *int, idxMap map[int]int) *TreeNode {
+	if inLeft > inRight {
+		return nil
+	}
+
+	rootVal := postorder[*postIdx]
+	root := TreeNode{Val: rootVal}
+	index := idxMap[rootVal]
+	*postIdx = (*postIdx) - 1
+
+	root.Right = helper(index+1, inRight, postorder, postIdx, idxMap)
+	root.Left = helper(inLeft, index-1, postorder, postIdx, idxMap)
+
+	return &root
+}
+
+func buildTree(inorder []int, postorder []int) *TreeNode {
+	postIdx := len(postorder) - 1
+	idx := 0
+	idxMap := map[int]int{}
+
+	for _, val := range inorder {
+		idxMap[val] = idx
+		idx++
+	}
+
+	return helper(0, len(inorder)-1, postorder, &postIdx, idxMap)
+}
+
+func main() {
+
+}
---
+++ medium/106. Construct Binary Tree from Inorder and Postorder Traversal/readme.md
@@ -0,0 +1,4 @@
+## 106. Construct Binary Tree from Inorder and Postorder Traversal
+
+Description:  
+https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
