10a9afb15678a970c67b487d39db58fa19be23ff
Артем Чаадаев <perfectgentleman.de@ya.ru>
2024-06-27 03:03:01 +0300

    idiotic solution for 3174

---
+++ easy/3174. Clear Digits/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"unicode"
+)
+
+func clearDigits(s string) string {
+	if s == "" {
+		return ""
+	}
+
+	prevRuneStr := []rune(s)
+	newRuneStr := make([]rune, 0, len(s))
+	last := false
+	steps := 0
+
+	for !last {
+		if len(prevRuneStr) == 0 {
+			return string(prevRuneStr)
+		}
+
+		for i, rn := range prevRuneStr {
+			if unicode.IsDigit(rn) {
+				if i != 0 {
+					if i != len(prevRuneStr)-1 {
+						newRuneStr = append(prevRuneStr[:i-1], prevRuneStr[i+1:]...)
+					} else {
+						newRuneStr = prevRuneStr[:i-1]
+					}
+				}
+
+				if i == len(prevRuneStr) {
+					last = true
+				}
+
+				break
+			} else {
+				if i == len(prevRuneStr)-1 {
+					return string(prevRuneStr)
+				}
+			}
+		}
+
+		prevRuneStr = newRuneStr
+		steps++
+	}
+
+	return string(prevRuneStr)
+}
+
+func main() {
+
+}
---
+++ easy/3174. Clear Digits/main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestClearDigits(t *testing.T) {
+	type testCase struct {
+		S              string
+		ExpectedResult string
+	}
+
+	cases := []testCase{
+		{
+			S:              "abc",
+			ExpectedResult: "abc",
+		},
+		{
+			S:              "cb34",
+			ExpectedResult: "",
+		},
+	}
+
+	for i := range cases {
+		got := clearDigits(cases[i].S)
+		if got != cases[i].ExpectedResult {
+			t.Errorf("S: %v, expected: %v got: %v", cases[i].S, cases[i].ExpectedResult, got)
+		}
+	}
+}
